9c349b128bd46ee9f8b4f2a50d934374162a948b
你çclang1996 <>
2020-07-15 11:23:15 +0800

    更新简历内容

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git commit -m '更新简历内容'
 #git push -f https://github.com/smallsunnyfox/smallsunnyfox.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f git@gitee.com:clang1996/myresume.git master:gh-pages
+ git push -f git@github.com:clang1996/myresume.git master:gh-pages
 
 git push
